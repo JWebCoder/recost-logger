19d9c2f39b9d22bf80e3ed53f457c466564a64f6
João Moura <joao.moura@b-i.com>
2019-03-13 10:16:24 +0000

    Added group logs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ let before = () => {}
 let after = () => {}
 if (check) {
   before = (state = {}, action = {}) => {
+    console.group('Context action:', action.type)
     console.log('State before action:', state)
     console.log('Full action:', action)
   }
 
   after = (state = {}) => {
     console.log('State after action:', state)
+    console.groupEnd()
   }
 }
 
